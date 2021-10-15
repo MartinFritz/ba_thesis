7a19a612a3801d8879d541a28febd7fb22ebaebc
MartinFritz <45102283+MartinFritz@users.noreply.github.com>
2021-10-15 22:28:31 +0200

    Add files via upload

--- jsonparser3.py
+++ jsonparser3.py
@@ -2,12 +2,17 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 
-#create benchmark class and fill it with json data
-#use pyplot to print every benchmark object
+import matplotlib
+from math import sqrt
 
-with open("/Users/shiro/hashtable_results_arm.json","r") as file:
+
+
+
+
+
+MARKERLIST = ['o','x','d','+','^','*', 's']
+with open("/Users/shiro/hashtable_benchmark_laptop.json","r") as file:
     data = json.load(file)
-
 labels = []
 cputime = []
 samplesize = []
@@ -43,14 +48,20 @@ for x in plotData:
     ylist.append([])
     labels.append(x)
     for y in plotData[x]:
-        xlist[len(xlist)-1].append(str(y[0]))
+        xlist[len(xlist)-1].append("{:.0e}".format(y[0]))
         ylist[len(ylist)-1].append(y[1])
 
+font = {'family' : 'serif',
+        'size'   : 10}
+
+matplotlib.rc('font', **font)
 for i in range(0,len(xlist)):
-    plt.plot(xlist[i],ylist[i], label=labels[i])
-    
+    plt.plot(xlist[i],ylist[i],marker=MARKERLIST[i], label=labels[i])
 plt.xlabel('element size')
 plt.ylabel('throughput in elements/ms')
+plt.tight_layout()
 plt.legend()
+
+plt.savefig("hashing_laptop.pdf")
 
 plt.show()
